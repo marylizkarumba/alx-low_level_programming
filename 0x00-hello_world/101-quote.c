2f8b82866a5ad0088aeac8abfba5a3320083046d
marylizkarumba <marylizkarumba791@gmail.com>
2022-03-10 12:23:24 -0800

    101 quote c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
  * Return: 1 if success.
  */
 int main(void)
-{
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
+{                
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, str1, 59);
 	return (1);
 }
