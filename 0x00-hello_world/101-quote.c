2f26a88e574c58e19afa5ed88adaa4f8ab085436
marylizkarumba <marylizkarumba791@gmail.com>
2022-03-10 11:31:58 -0800

    101 quote c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 61);
+        fwrite(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+        
 	return (1);
 }
