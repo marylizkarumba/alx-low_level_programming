162b3cb27f7aa8aa75f1ddabaea74aa89d5a3225
marylizkarumba <marylizkarumba791@gmail.com>
2022-03-17 13:36:22 -0700

    0 isupper c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,9 +3,9 @@
 /**
  * _isupper - define if a character is uppercase
  *
- * Description: Prints the alphabet with _putchar
- * @c: charater
- * Return: void
+ * @c: uppercase character
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
  */
 
 int _isupper(int c)
