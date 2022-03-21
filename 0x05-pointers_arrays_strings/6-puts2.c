8115241445069737137211620bed2d3b1009f075
marylizkarumba <marylizkarumba791@gmail.com>
2022-03-21 08:57:09 -0700

    6 puts2 c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a  string to stdout
+ * puts2 - prints every other character of a  string starting with the first character
  * @str: string to print
  *
  * Return: void
  */
-void puts2(char *str);
+void puts2(char *str)
 {
 	int i = 0;
 
