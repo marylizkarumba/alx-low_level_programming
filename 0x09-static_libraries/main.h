ab6c4456b15c05f9e16d2601f3b3b750bfdb029a
marylizkarumba <marylizkarumba791@gmail.com>
2022-04-01 13:15:21 -0700

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,11 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
 	
 int _putchar(char c);
 int _islower(int c);
